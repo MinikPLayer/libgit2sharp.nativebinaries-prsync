16c398e19cd51f0afd73da9cccf6ff3a41b3d591
Michał Tomecki <minexplaypl@gmail.com>
2024-04-16 20:46:47 +0200

    Fixed a typo

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-RID=x64 ./build.libgit2.sh && RID=x86 ./build.libgit2.sh && RID=amd64 ./build.libgit2.sh
+RID=x64 ./build.libgit2.sh && RID=x86 ./build.libgit2.sh && RID=arm64 ./build.libgit2.sh
